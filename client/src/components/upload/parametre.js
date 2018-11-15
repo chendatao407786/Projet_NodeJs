508bb3746387ff519d36be6cd88ea66fab1c0dd9
CHEN Datao <chendatao1126@gmail.com>
2018-11-16 00:13:43 +0100

    Update client/src/components/upload/parametre.js

--- client/src/components/upload/parametre.js
+++ client/src/components/upload/parametre.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import $ from 'jquery';
 
 const font_weight = {
     fontWeight: '100'
@@ -11,49 +10,7 @@ const add_button = {
     fontSize: '100%',
     fontWeight:'400'
 };
-// class Parametre extends Component{
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//             controler: null,
-//             default:null,
-//             min:null,
-//             max:null
-//         }
-//     }
-//     handleInputChanged = (event) => {
-//         this.setState({
-//             [event.target.name]: event.target.value
-//         },()=>{
-//             console.log(this.state);
-//         })
-//     }
 
-//     handleOnClick = (event) => {
-//         $('#parametreSubmit').attr("disabled", "disabled");
-//     }
-//     render(){
-//         return(
-//             <div className="form-row" id="seller">
-//                 <div className="form-group col-md-2.5">
-//                     <input style={font_weight} type="text" className="form-control" id="sellerName" placeholder="Control" name="controler" />
-//                 </div>
-//                 <div className="form-group col-md-2.5">
-//                     <input style={font_weight} type="text" className="form-control" id="sellerUrl" placeholder="Default" name="default" />
-//                 </div>
-//                 <div className="form-group col-md-2.5">
-//                     <input style={font_weight} type="text" className="form-control" id="sellerUrl" placeholder="Min" name="min" />
-//                 </div>
-//                 <div className="form-group col-md-2.5">
-//                     <input style={font_weight} type="text" className="form-control" id="sellerUrl" placeholder="Max" name="max" />
-//                 </div>
-//                 <div className="form-group col-md-1">
-//                     <button id="parametreSubmit" style={add_button} className="btn btn-primary btn-sm" onClick={this.handleOnClick}>+</button>
-//                 </div>  
-//             </div>
-//         )
-//     }
-// }
 function Parametre(props){
     return(
         <div className="form-row" id="parametre">
