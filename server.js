c0e5d684562192b1901aa22a0044a7be6bd7f2b3
CHEN Datao <chendatao1126@gmail.com>
2018-11-15 18:35:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const app = express();
 const plugin = require('./routers/api/plugin');
+const url = 'mongodb://chendatao:bonjour1941@ds039301.mlab.com:39301/plugin_store';
+
 app.use(bodyParser.json());
+app.use(methodOverride('_method'))
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
     next();
 });
-const url = 'mongodb://localhost:27017/dataobeaugosse';
+
 mongoose
     .connect(url)
     .then(() => console.log('connected'))
