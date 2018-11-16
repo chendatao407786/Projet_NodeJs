27de44ae30867f9a0587598927d62c5b54701a50
CHEN Datao <chendatao1126@gmail.com>
2018-11-16 10:25:10 +0100

    Update client/src/components/displayPlugins/display.js

---
+++ client/src/components/displayPlugins/display.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+class Display extends Component{
+    constructor(props){
+        super(props);
+        // this.state{
+
+        // }
+    }
+    render(){
+        return(
+            "todo"
+        )
+    }
+}
+
+export default Display;
