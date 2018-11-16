9f3128d1f60da15e7a5ce28645439d44c892ef89
CHEN Datao <chendatao1126@gmail.com>
2018-11-16 10:25:07 +0100

    Update client/src/App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// import 'bootstrap/dist/js/bootstrap';
-import Upload from './components/upload/upload'
+import {BrowserRouter,Route} from 'react-router-dom';
+import Upload from './components/upload/upload';
+import Display from './components/displayPlugins/display';
 
 class App extends Component {
   render() {
     return (
-        <Upload/>
+        <BrowserRouter>
+          <div>
+            <Route path="/" component={Display} exact/>
+            <Route path="/upload" component={Upload}/>
+          </div>
+          
+        </BrowserRouter>
+        
     );
   }
 }
