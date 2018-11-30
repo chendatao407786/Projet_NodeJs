5535c991c0de236f9168a932c23271598135b0f7
CHEN Datao <chendatao1126@gmail.com>
2018-11-30 14:36:00 +0100

    Update pluginItem.js

--- client/src/components/displayPlugins/pluginItem.js
+++ client/src/components/displayPlugins/pluginItem.js
@@ -1,55 +1,61 @@
-import React,{Component} from 'react';
-// import i from '../../../../storage/images/'
-require('./css/index.css');
-class PluginItem extends Component{
-    constructor(props){
-        super(props);
-        this.state={
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-        }
-    };
+require('./css/index.css');
+class PluginItem extends Component {
+    // constructor(props) {
+    //     super(props);
+    // };
     inspiredBy = () => {
-        let description =this.props.plugin.description;
-        if(description.includes("classic")){
+        let description = this.props.plugin.description;
+        if (description.includes("classic")) {
             let inspiration;
             description = description.split("classic");
-            for(let element of description){
-                let ele = element.substring(0,40);
-                if(ele.includes("(*)")){
+            for (let element of description) {
+                let ele = element.substring(0, 40);
+                if (ele.includes("(*)")) {
                     inspiration = ele.split("(*)")[0];
                     break;
                 }
             }
-            // description = description.split("(*)")[0];
             return inspiration;
-        }else{
+        } else {
             return "";
         }
-        
     }
-    
-    render(){
-        if(this.props.isEmpty === "false"){
-            return(
+    redirect = () => {
+        return (
+            <Link
+                className="button"
+                to={{
+                    pathname: '/plugindetail/' + this.props.plugin._id,
+                    data: this.props.plugin
+                }}>
+                DETAILS
+            </Link>
+        )
+    }
+    render() {
+        console.log(this.props);
+        
+        if (this.props.isEmpty === "false") {
+            let detailButton = this.redirect();
+            return (
                 <div className="plugin-item">
                     <div className="detail">
                         <span id="title">{this.props.plugin.creator}</span>
                         <img src={this.props.plugin.imageUrl} alt=''></img>
                         <span className="sub-description">Inspired by <this.inspiredBy></this.inspiredBy></span>
-                        <span className="button">DETAILS</span>
-                    </div>                
+                        {detailButton}
+                    </div>
                 </div>
             );
-        }else{
-            return(
-                <div className="plugin-item empty">             
+        } else {
+            return (
+                <div className="plugin-item empty">
                 </div>
             )
         }
-
-        
     }
-    
 }
-
 export default PluginItem;
