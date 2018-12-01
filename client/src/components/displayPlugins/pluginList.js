9a49c8a9c0203658a9abe6e7a4e640a58c365e0d
CHEN Datao <chendatao1126@gmail.com>
2018-12-01 01:42:05 +0100

    Update pluginList.js

--- client/src/components/displayPlugins/pluginList.js
+++ client/src/components/displayPlugins/pluginList.js
@@ -7,32 +7,12 @@ import store from '../../store';
 require('./css/index.css');
 class PluginList extends Component {
 
-    // constructor(pros) {
-    //     super(pros);
-    //     this.state = {
-    //         plugins: []
-    //     }
-    // }
-    // getPluginListData = () => {
-    //     fetch('/api/plugin')
-    //         .then(res => {
-    //             return res.json()
-    //         }).then(res => {
-    //             let newData = [];
-    //             res.forEach(element => {
-    //                 newData.push(element);
-    //             });
-    //             this.setState({
-    //                 plugins: newData
-    //             }, () => console.log(this.state))
-    //         })
-    // }
     componentWillMount() {
         console.log("Component will mount");
         // this.getPluginListData();
         this.props.getPlugins();
-        console.log(store.getState());
     }
+    
     render() {
         let listPlugins = this.props.plugin.plugins.map((element, index) => {
             return <PluginItem isEmpty="false" plugin={element} key={index}></PluginItem> 
