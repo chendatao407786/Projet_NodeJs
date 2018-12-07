29c4708cddc8822de9be38ee4e288d73bf092dad
CHEN Datao <chendatao1126@gmail.com>
2018-12-07 23:53:54 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,10 @@ import Upload from './components/upload/upload';
 import PluginList from './components/displayPlugins/pluginList';
 import PluginDetail from './components/detail/plugin';
 import PluginStore from './components/pluginStore/pluginStore';
-import Login from './components/login/login';
+import Login from './components/auth/login';
 import { Provider } from 'react-redux';
 import store from './store';
+import SignUp from './components/auth/signUp';
 
 class App extends Component {
   logger = () => {
@@ -25,6 +26,7 @@ class App extends Component {
             <Route path="/plugin-store" component={PluginStore} exact/>
             <Route path="/plugin-store/:tag" component={PluginStore}/>
             <Route path="/login" component={Login}/>
+            <Route path="/sign-up" component={SignUp}/>
           </div>
         </BrowserRouter>
       </Provider>
