ea607b64c0e06de4575218aeb7632af7eed3e70e
CHEN Datao <chendatao1126@gmail.com>
2018-12-07 23:54:40 +0100

    Update plugin.js

--- routers/api/plugin.js
+++ routers/api/plugin.js
@@ -79,7 +79,7 @@ router
         Plugin
             .findById(req.params.id)
             .then(plugin => plugin.remove().then(() => res.json({ success: true })))
-            .catch(err => res.status(404).json({ success: false }));
+            .catch(err => res.status(404).json({ err }));
     })
 
 
