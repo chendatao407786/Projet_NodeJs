62336515b916acab8892123ad8499cc6ce50d782
CHEN Datao <chendatao1126@gmail.com>
2018-12-10 11:46:07 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Upload from './components/upload/upload';
 import PluginList from './components/displayPlugins/pluginList';
 import PluginDetail from './components/detail/plugin';
@@ -10,6 +10,8 @@ import Login from './components/auth/login';
 import { Provider } from 'react-redux';
 import store from './store';
 import SignUp from './components/auth/signUp';
+import Error from './components/error';
+import Navigation from './components/navigator/navigation';
 
 class App extends Component {
   // logger = () => {
@@ -17,19 +19,28 @@ class App extends Component {
   // }
   render() {
     return (
+
+
+
       <Provider store={store}>
         <BrowserRouter>
           <div>
-            <Route path="/" component={PluginList} exact />
-            <Route path="/plugins/upload" component={Upload} exact />
-            <Route path="/plugin/:id" component={PluginDetail} exact/>
-            <Route path="/plugin-store" component={PluginStore} exact/>
-            <Route path="/plugin-store/:tag" component={PluginStore}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/sign-up" component={SignUp}/>
+            <Navigation />
+            <Switch>
+              <Route path="/" component={PluginList} exact />
+              <Route path="/plugins/upload" component={Upload} exact />
+              <Route path="/plugin/:id" component={PluginDetail} exact />
+              <Route path="/plugin-store" component={PluginStore} exact />
+              <Route path="/plugin-store/:tag" component={PluginStore} />
+              <Route path="/login" component={Login} />"
+              <Route path="/sign-up" component={SignUp} />
+              <Route component={Error} />
+            </Switch>
           </div>
         </BrowserRouter>
       </Provider>
+
+
     );
   }
 }
