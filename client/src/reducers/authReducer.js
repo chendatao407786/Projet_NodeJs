6a4ea8333706e00b61f663fa8a6995fa9e741bb1
CHEN Datao <chendatao1126@gmail.com>
2018-12-12 21:39:41 +0100

    Update authReducer.js

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,4 +1,5 @@
-import {SET_CURRENT_USER } from '../actions/types'
+import {SET_CURRENT_USER } from '../actions/types';
+import {isEmpty} from 'lodash';
 const initialState = {
     user:{},
     isSignIn:false
@@ -6,12 +7,15 @@ const initialState = {
 const auth = (state = initialState, action = {}) => {
     switch(action.type){
         case SET_CURRENT_USER :
+            console.log(!isEmpty(action.user));
+            
             return {
                 ...state,
                 user:action.user,
-                isSignIn: true
+                isSignIn: !isEmpty(action.user)
             }
         default: return state;
     }
 }
+
 export default auth;
