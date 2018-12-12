49b3c28aac780a22676d6a1f5da2c77d246c037d
CHEN Datao <chendatao1126@gmail.com>
2018-12-12 13:54:48 +0100

    Update plugin.js

--- client/src/components/detail/plugin.js
+++ client/src/components/detail/plugin.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
+// import axios from 'axios';
 require('./css/pluginDetail.css');
 
 class PluginDetail extends Component {
