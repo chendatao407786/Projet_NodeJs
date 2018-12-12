8f12f91e29d025425c530fa42c7c1e765b9b8ae7
CHEN Datao <chendatao1126@gmail.com>
2018-12-12 21:39:47 +0100

    Create requireLogin.js

---
+++ client/src/utils/requireLogin.js
@@ -0,0 +1,27 @@
+import React,{Component} from 'react';
+import {connect} from 'react-redux';
+import PropTypes from 'prop-types';
+export default function(ComponentSecured) {
+    class Authenticate extends Component{
+        static contextTypes = {
+            router: PropTypes.object
+        }
+        componentWillMount(){
+            if(!this.props.isAuth){
+                this.context.router.history.push("/login");
+            }
+        }
+        render(){
+            return(
+                <ComponentSecured {...this.props}/>
+            )
+        }
+    }
+    Authenticate.propTypes = {
+        isAuth: PropTypes.bool.isRequired
+    }
+    const mapStateToProps = (state) => ({
+        isAuth: state.auth.isSignIn
+    })
+    return connect(mapStateToProps)(Authenticate);
+}
