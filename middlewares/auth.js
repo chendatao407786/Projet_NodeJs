f4df96a169086fa333280168a6d538d7fc9fbff8
CHEN Datao <chendatao1126@gmail.com>
2018-12-12 21:39:51 +0100

    Update auth.js

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -12,35 +12,9 @@ function verifyToken(req, res, next) {
             if (err) {
                 res.status(401).json({ error: 'Failed to authenticate' });
             } else {
-                // // User.findById(decoded.id)
-                // console.log(decoded);
-                // User
-                //     .findById(decoded._id)
-                //     .then(
-                //         user => {
-                //                 if (!user) {
-                //                     res.status(404).json({ error: 'No such user' });
-                //                 } else {
-                //                     req.currentUser = user;
-                //                     next();
-                //                 }
-                //             }
-                //     )
                 next();
-                // User.query({
-                //     where: { id: decoded.id },
-                //     select: ['email', 'id', 'username']
-                // }).fetch().then(user => {
-                //     if (!user) {
-                //         res.status(404).json({ error: 'No such user' });
-                //     } else {
-                //         req.currentUser = user;
-                //         next();
-                //     }
-                // })
             }
         })
-        // res.send("connected");
     } else {
         res.status(403).json({
             error: 'No token provided'
