f40cbfe987dbf6e7e04ec2706cef479b376629aa
CHEN Datao <chendatao1126@gmail.com>
2018-12-14 01:47:13 +0100

    Update types.js

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -8,3 +8,6 @@ export const LOADING_PLUGIN = 'LOADING_PLUGIN';
 export const ADD_USER = 'ADD_USER';
 export const ADD_FLASH_MESSAGE = 'ADD_FLASH_MESSAGE';
 export const SET_CURRENT_USER = 'SET_CURRENT_USER';
+
+export const LOADING_USER_DATA = 'LOADING_USER_DATA';
+export const GET_MY_PLUGINS = 'GET_MY_PLUGINS';
