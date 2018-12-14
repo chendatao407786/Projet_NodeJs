af7706f959d78a3368c9bca41e49bf146ab206c0
CHEN Datao <chendatao1126@gmail.com>
2018-12-14 11:42:02 +0100

    Update myPlugins.js

--- client/src/components/myAccount/myPlugins.js
+++ client/src/components/myAccount/myPlugins.js
@@ -7,19 +7,13 @@ require('./myAccount.css');
 
 class MyPlugins extends Component {
     componentWillMount(){
-        this.props.getMyPlugins(this.props.userId);
+        this.props.getMyPlugins(this.props.auth.user[0]._id);
     }
     render(){
-        // console.log(this.props.userData.userData);
-        // console.log(this.props.userId);
         let plugins = this.props.userData.userData;
-        console.log(plugins);
-        
         let myPluginsList = plugins.map((element,index) => {
             return<MyPluginItem isEmpty="false" plugin={element} key={index}></MyPluginItem> 
-            // console.log(element);
         })
-
         return(
             <div className="store-plugins-container">{myPluginsList}</div>
         )
@@ -29,10 +23,10 @@ class MyPlugins extends Component {
 MyPlugins.propTypes = {
     getMyPlugins:PropTypes.func.isRequired,
     userData:PropTypes.object.isRequired,
-    userId:PropTypes.string.isRequired
+    auth: PropTypes.object.isRequired
 }
 const mapStateToProps = (state) => ({
-    userId:state.auth.user[0]._id,
+    auth: state.auth,
     userData:state.userData
 })
 export default connect(mapStateToProps,{getMyPlugins})(MyPlugins);
