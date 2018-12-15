8904dcd4b8dddd31eec9f1df6de0ec4fbd895351
CHEN Datao <chendatao1126@gmail.com>
2018-12-16 00:40:52 +0100

    Update myPlugins.js

--- client/src/components/myAccount/myPlugins.js
+++ client/src/components/myAccount/myPlugins.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {getMyPlugins} from '../../actions/userDataAction';
+import {getMyPlugins,deletePlugin} from '../../actions/userDataAction';
 import PropTypes from 'prop-types';
 import MyPluginItem from './myPluginItem';
 require('./myAccount.css');
@@ -10,18 +10,25 @@ class MyPlugins extends Component {
         this.props.getMyPlugins(this.props.auth.user[0]._id);
     }
     render(){
-        let plugins = this.props.userData.userData;
+        if(this.props.auth.user[0]){
+            let plugins = this.props.userData.userData;
         let myPluginsList = plugins.map((element,index) => {
-            return<MyPluginItem isEmpty="false" plugin={element} key={index}></MyPluginItem> 
+            return<MyPluginItem isEmpty="false" plugin={element} key={index} userId={this.props.auth.user[0]._id} deletePlugin={this.props.deletePlugin}></MyPluginItem> 
         })
         return(
             <div className="store-plugins-container">{myPluginsList}</div>
         )
+        }else{
+            return(
+                <h1>I have no idea where i am</h1>
+            )
+        }
     }
 }
 
 MyPlugins.propTypes = {
     getMyPlugins:PropTypes.func.isRequired,
+    deletePlugin:PropTypes.func.isRequired,
     userData:PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired
 }
@@ -29,4 +36,4 @@ const mapStateToProps = (state) => ({
     auth: state.auth,
     userData:state.userData
 })
-export default connect(mapStateToProps,{getMyPlugins})(MyPlugins);
+export default connect(mapStateToProps,{getMyPlugins,deletePlugin})(MyPlugins);
