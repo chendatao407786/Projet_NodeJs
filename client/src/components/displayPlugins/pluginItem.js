227934ad6d82fd5866053b80a75ef117c97c82b5
CHEN Datao <chendatao1126@gmail.com>
2018-12-16 02:18:43 +0100

    Update pluginItem.js

--- client/src/components/displayPlugins/pluginItem.js
+++ client/src/components/displayPlugins/pluginItem.js
@@ -3,26 +3,6 @@ import { Link } from 'react-router-dom';
 
 require('./css/index.css');
 class PluginItem extends Component {
-    // constructor(props) {
-    //     super(props);
-    // };
-    // inspiredBy = () => {
-    //     let description = this.props.plugin.description;
-    //     if (description.includes("classic")) {
-    //         let inspiration;
-    //         description = description.split("classic");
-    //         for (let element of description) {
-    //             let ele = element.substring(0, 40);
-    //             if (ele.includes("(*)")) {
-    //                 inspiration = ele.split("(*)")[0];
-    //                 break;
-    //             }
-    //         }
-    //         return inspiration;
-    //     } else {
-    //         return "";
-    //     }
-    // }
     redirect = () => {
         return (
             <Link
